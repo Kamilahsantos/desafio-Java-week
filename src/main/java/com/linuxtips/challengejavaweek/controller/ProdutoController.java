f74c8f5942c5044b2d1e3ca4bf1f4f6159597a1d
Kamila de fatima santos oliveira <kahsantos89@gmail.com>
2023-03-21 00:00:52 -0300

    Update ProdutoController.java

--- src/main/java/com/linuxtips/challengejavaweek/controller/ProdutoController.java
+++ src/main/java/com/linuxtips/challengejavaweek/controller/ProdutoController.java
@@ -13,39 +13,5 @@ import java.util.List;
 @RequestMapping("/api/")
 public class ProdutoController {
 
-    public ProdutoController(ProdutoService produtoService) {
-        this.produtoService = produtoService;
-    }
-
-    ProdutoService produtoService;
-
-    @PostMapping("/produtos")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Produto criarProduto(@RequestBody Produto produto) {
-        return produtoService.criarProduto(produto);
-    }
-
-    @GetMapping("/produtos")
-    @ResponseStatus(HttpStatus.OK)
-    public List<Produto> listarProdutos() {
-        return produtoService.listarProdutos();
-    }
-
-    @GetMapping("/produtos/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<Produto> buscarProdutoPeloId(@PathVariable(value = "id") Long id) {
-        return produtoService.buscarProdutoPeloId(id);
-    }
-
-    @PutMapping("/produtos/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<Produto> atualizarProdutoPeloId(@PathVariable(value = "id") Long id, @RequestBody Produto produto) {
-        return produtoService.atualizarProdutoPeloId(produto, id);
-    }
-
-    @DeleteMapping("/produtos/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ResponseEntity<Object> apagarPeloId(@PathVariable(value = "id") Long id) {
-        return produtoService.apagarPeloId(id);
-    }
+    
 }
